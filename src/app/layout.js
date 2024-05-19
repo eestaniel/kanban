15044a9aa28e9edb9e2066901a752cc7e032d6b9
EEstaniel <edwineestaniel93@gmail.com>
2024-05-19 15:11:57 -0700

    fix: fix proper import

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,16 @@ import "./reset.css";
 import "./globals.css";
 
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
-        <body>{children}</body>
+    <head>
+      <meta charSet="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Kanban</title>
+      <link rel="icon" href="#"/>
+    </head>
+    <body>{children}</body>
     </html>
   );
 }
