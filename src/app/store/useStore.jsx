8a9567e769147930f4b79eaa9ba76972cb4ac787
EEstaniel <edwineestaniel93@gmail.com>
2024-05-18 21:06:24 -0700

    feat(navbar): add logic to disable button based on board columns

--- src/app/store/useStore.jsx
+++ src/app/store/useStore.jsx
@@ -1,20 +1,36 @@
 import create from 'zustand';
 
 const useStore = create((set, get) => ({
-  boards: {},
+  /*boards: {
+  *   board_id: {
+  *     title: '',
+  *     columns: {
+  *       column_id: {
+  *         title: '',
+  *         description: '',
+  *         subtasks: [],
+  *         current_state: ''
+  *     }
+  *     */
+  boards: {}, //object with all boards containing columns, subtasks, etc.
+  selectedBoardId: null, // id of the board currently being viewed
+
+  // UI state
   isDarkMode: false,
   isModalOpen: false,
+
+  // Modal state
   modalType: '',
 
   printBoard: () => console.log(get().state),
 
   getBoardAmount: () => Object.keys(get().boards).length,
 
-  toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+  toggleDarkMode: () => set((state) => ({isDarkMode: !state.isDarkMode})),
 
-  activateModal: (modalType) => set(() => ({ isModalOpen: true, modalType })),
+  activateModal: (modalType) => set(() => ({isModalOpen: true, modalType})),
 
-  closeModal: () => set(() => ({ isModalOpen: false, modalType: '' }))
+  closeModal: () => set(() => ({isModalOpen: false, modalType: ''}))
 }));
 
 export default useStore;
