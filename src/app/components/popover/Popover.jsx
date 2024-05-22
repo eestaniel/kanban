b97065ac78e99f514b86895ff6d14e2cad8f5c98
EEstaniel <edwineestaniel93@gmail.com>
2024-05-22 01:38:21 -0700

    refactor: refactor popver

--- src/app/components/popover/Popover.jsx
+++ src/app/components/popover/Popover.jsx
@@ -1,30 +1,53 @@
 import './popover.css'
 import useStore from "@/app/store/useStore";
+import { useEffect, useRef, useState } from "react";
 
+const Popover = ({isPopoverOpen, setIsPopoverOpen}) => {
 
-const Popover = ({setIsPopoverOpen}) => {
+  const popoverRef = useRef(null);
+
   const { activateModal } = useStore(state => ({
     activateModal: state.activateModal
-  }))
+  }));
 
-    const handleEditBoard = () => {
+  const handleEditBoard = () => {
     setIsPopoverOpen(false);
-    activateModal('edit-board',);
+    activateModal('edit-board');
   }
 
   const handleDeleteBoard = () => {
-    // add confirmation later
-    /*deleteBoard(activeBoard.board_id);*/
     activateModal('delete');
     setIsPopoverOpen(false);
   }
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (popoverRef.current && !popoverRef.current.contains(event.target)) {
+        setIsPopoverOpen(false);
+      }
+    }
+
+    if (isPopoverOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isPopoverOpen]);
+
+
+
   return (
     <div className="popover-container">
-      <div className="popover-content">
-        <div className="popover-item" onClick={()=>handleEditBoard()}>Edit Board</div>
-        <div className="popover-item" onClick={()=>handleDeleteBoard()}>Delete Board</div>
-      </div>
+      {isPopoverOpen && (
+        <div className="popover-ref-container" ref={popoverRef}>
+          <div className="popover-content">
+            <div className="popover-item" onClick={handleEditBoard}>Edit Board</div>
+            <div className="popover-item popover-delete" onClick={handleDeleteBoard}>Delete Board</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
