891ddfff4e3db63906998e129c4f62f9e7516e55
EEstaniel <edwineestaniel93@gmail.com>
2024-05-24 16:24:31 -0700

    refactor: refactor popover
    State Destructuring: Improved readability by destructuring state and actions from useStore.
    
    Inline Functions: Moved inline functions outside of JSX to improve readability and performance.
    
    Effect Hook: Simplified the useEffect hook by separating the event handler function.
    
    Wrapped the handleClickOutside function with useCallback to memoize it and prevent unnecessary re-renders.

--- src/app/components/popover/Popover.jsx
+++ src/app/components/popover/Popover.jsx
@@ -1,65 +1,58 @@
-import './popover.css'
+import './popover.css';
 import useStore from "@/app/store/useStore";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback } from "react";
 
-const Popover = ({isPopoverOpen, setIsPopoverOpen, popoverType, task}) => {
-
+const Popover = ({ isPopoverOpen, setIsPopoverOpen, popoverType, task }) => {
   const popoverRef = useRef(null);
 
-  const { activateModal } = useStore(state => ({
+  const { activateModal } = useStore((state) => ({
     activateModal: state.activateModal,
   }));
-
 
   const handleEditBoard = () => {
     setIsPopoverOpen(false);
     activateModal('edit-board');
-  }
+  };
 
   const handleDeleteBoard = () => {
     activateModal('delete');
     setIsPopoverOpen(false);
-  }
+  };
 
   const handleEditTask = () => {
     setIsPopoverOpen(false);
     activateModal('edit-task', task);
+  };
 
-
-  }
+  const handleClickOutside = useCallback((event) => {
+    if (popoverRef.current && !popoverRef.current.contains(event.target)) {
+      setIsPopoverOpen(false);
+    }
+  }, [setIsPopoverOpen]);
 
   useEffect(() => {
-    function handleClickOutside(event) {
-      if (popoverRef.current && !popoverRef.current.contains(event.target)) {
-        setIsPopoverOpen(false);
-      }
-    }
-
     if (isPopoverOpen) {
       document.addEventListener('mousedown', handleClickOutside);
     }
-
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [isPopoverOpen, setIsPopoverOpen]);
-
-
+  }, [isPopoverOpen, handleClickOutside]);
 
   return (
     <div className="popover-container">
       {isPopoverOpen && (
-        <div className={`popover-ref-container ${popoverType==='nav-task' && 'popover-task'}`} ref={popoverRef}>
+        <div className={`popover-ref-container ${popoverType === 'nav-task' ? 'popover-task' : ''}`} ref={popoverRef}>
           <div className="popover-content">
             {popoverType === 'nav-menu' && (
               <ul className="popover-menu">
-                <li className="popover-menu-item" onClick={()=>handleEditBoard()}>Edit Board</li>
+                <li className="popover-menu-item" onClick={handleEditBoard}>Edit Board</li>
                 <li className="popover-menu-item popover-delete" onClick={handleDeleteBoard}>Delete Board</li>
               </ul>
             )}
             {popoverType === 'nav-task' && (
               <ul className="popover-menu">
-                <li className="popover-menu-item" onClick={()=>handleEditTask()}>Edit Task</li>
+                <li className="popover-menu-item" onClick={handleEditTask}>Edit Task</li>
                 <li className="popover-menu-item popover-delete" onClick={handleDeleteBoard}>Delete Task</li>
               </ul>
             )}
